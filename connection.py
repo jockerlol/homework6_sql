5084cb05d1ac32e971bca4997859647bf16d3319
jockerlol <jockerlol1@gmail.com>
2023-09-17 15:44:05 +0300

    first commit

---
+++ connection.py
@@ -0,0 +1,18 @@
+import sqlite3
+from contextlib import contextmanager
+
+DATABASE = "MyUniversityDB.db"
+
+
+@contextmanager
+def create_connection():
+    connection = None
+    try:
+        connection = sqlite3.connect(DATABASE)
+        yield connection
+        connection.commit()
+    except Exception as exp:
+        print(exp)
+        connection.rollback()
+    finally:
+        connection.close()
